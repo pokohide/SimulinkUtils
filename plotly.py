dcd8711b57f7346fffd98a9fa41cfbe4f51caf41
pokohide <hyde31415@gmail.com>
2017-11-08 15:20:40 +0900

    add example config

--- plotly.py
+++ plotly.py
@@ -1,7 +1,7 @@
 from lib import plotly as Plotly
 
 if __name__ == "__main__":
-    # plot = Plotly.Plotly("./examples/tmp_sch_results.csv")
+    # plot = Plotly.Plotly("./examples/tmp_sch_results.csv", { "showTitle": False })
     plot = Plotly.Plotly([
         "./examples/singlerate_sensorless_100us.csv",
         "./examples/singlerate_sensorless.csv"

b0779a25cb98a31d73f53c41e6b4db84138b08e1
pokohide <hyde31415@gmail.com>
2017-11-08 15:06:04 +0900

    correspond multiple input

--- lib/plotly.py
+++ lib/plotly.py
@@ -11,46 +11,46 @@ class Plotly:
     HEIGHT = 1.0
 
     def __init__(self, fname):
-        self.data = self._load_csv(fname)
-        self._ax = plt.gca()
-        self._fig = self._ax.figure
-        self._annotations = []
+        self.csvs = self._load(fname)
+        count = len(self.csvs)
+
+        self._fig, self._ax = plt.subplots(count, 1)
+        if count is 1: self._ax = [self._ax]
+        self._annotations = [[] for i in range(count)]
+        self._init_colors()
 
     def plot(self):
-        COLORS = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
-             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
-             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
-             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
-             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+        for index, csv in enumerate(self.csvs):
+            self._set_graph(index, csv)
+        self._fig.canvas.mpl_connect('button_press_event', self._on_press)
+        plt.show()
 
-        for i in range(len(COLORS)):
-            r, g, b = COLORS[i]
-            COLORS[i] = (r / 255., g / 255., b / 255.)
-
-        for item in self.data:
+    def _set_graph(self, index, csv):
+        for item in csv:
             core = item[0]
             data = item[1].data()
             xs = list(map(lambda x: (x["start"], x["end"] - x["start"]), data))
             colors = list(map(lambda x: "red", data))
-            colors = [COLORS[i % 20] for (i, x) in enumerate(data)]
-            # hatches = [("//", "\\\\")[i % 2] for (i, x) in enumerate(data)]
-            self._hatch_broken_bar(xs, (core * 1.5 + 1, 1), facecolors=colors)
+            colors = [self.colors[i % 20] for (i, x) in enumerate(data)]
+            self._hatch_broken_bar(index, xs, (core * 1.5 + 1, 1), facecolors=colors)
             for block in data:
-                width, annotation = self._get_annotation(block)
-                self._annotations.append((width, annotation))
-        self._ax.set_ylim(0, 1.5 * self.max_core + 1.5 + self.__class__.HEIGHT / 2)
-        self._ax.set_xlim(0, self.endtime)
-        self._ax.set_yticks([(i * 1.5 + self.__class__.HEIGHT) for i in range(self.max_core + 1)])
-        self._ax.set_yticklabels([("core " + str(i)) for i in range(self.max_core + 1)])
-        self._ax.grid(True)
+                width, annotation = self._get_annotation(index, block)
+                self._annotations[index].append((width, annotation))
 
-        self._fig.canvas.mpl_connect('button_press_event', self._on_press)
-        plt.show()
+        self._ax[index].set_ylim(0, 1.5 * self.max_core + 1.5 + self.__class__.HEIGHT / 2)
+        self._ax[index].set_xlim(0, self.endtime)
+        self._ax[index].set_yticks([(i * 1.5 + self.__class__.HEIGHT) for i in range(self.max_core + 1)])
+        self._ax[index].set_yticklabels([("core " + str(i)) for i in range(self.max_core + 1)])
+        self._ax[index].grid(True)
 
     def _on_press(self, event):
         visibility_changed = False
+        index = 0
+        for i, ax in enumerate(self._ax):
+            if ax == event.inaxes: index = i
+
         x, y = event.xdata, event.ydata
-        for width, annotation in self._annotations:
+        for width, annotation in self._annotations[index]:
             should_be_visible = self._is_contain(annotation.xy, width, self.__class__.HEIGHT, x, y)
 
             if should_be_visible != annotation.get_visible():
@@ -67,14 +67,14 @@ class Plotly:
         dy = abs(ty - y)
         return (dx < width / 2) and (dy < height / 2)
 
-    def _get_annotation(self, block):
+    def _get_annotation(self, index, block):
         """
         ブロックのホバー時にブロック名を表示する
         """
         width = block["end"] - block["start"]
         x = block["start"] + width / 2.0
         y = block["core"] * 1.5 + 1
-        annotation = self._ax.annotate(
+        annotation = self._ax[index].annotate(
             block["name"], xy = (x, y), xycoords="data",
             xytext=(x + 10, y + self.__class__.HEIGHT / 2), textcoords="data",
             bbox=dict(boxstyle="round", facecolor="w", edgecolor="0.5", alpha=0.9),
@@ -83,14 +83,24 @@ class Plotly:
         annotation.set_visible(False)
         return (width, annotation)
 
-    def _hatch_broken_bar(self, xs, y, **kw):
+    def _hatch_broken_bar(self, index, xs, y, **kw):
         hatches = kw.pop("hatches", [None] * len(xs))
         facecolors = kw.pop("facecolors", [None] * len(xs))
         edgecolors = kw.pop("edgecolors", [None] * len(xs))
 
         for i, x in enumerate(xs):
-            self._ax.barh(bottom = y[0], width = x[1], height = y[1], left = x[0],
+            self._ax[index].barh(bottom = y[0], width = x[1], height = y[1], left = x[0],
                     facecolor = facecolors[i], edgecolor = edgecolors[i], hatch = hatches[i])
+
+    def _load(self, fnames):
+        if isinstance(fnames, list):
+            csvs = []
+            for fname in fnames:
+                csv = self._load_csv(fname)
+                csvs.append(csv)
+            return csvs
+        elif isinstance(fnames, str):
+            return [self._load_csv(fnames)]
 
     def _load_csv(self, fname):
         cores = {}
@@ -112,6 +122,19 @@ class Plotly:
         for core in cores.values(): core.sorted("start") # startTimeでソート
 
         return sorted(cores.items())
+
+    def _init_colors(self):
+        def to_rgb(color):
+            r, g, b = color
+            return (r / 255., g / 255., b / 255.)
+
+        COLORS = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+
+        self.colors = list(map(lambda color: to_rgb(color), COLORS ))
 
     def _format(self, data):
         """格納する配列データを整形する"""
--- plotly.py
+++ plotly.py
@@ -1,10 +1,9 @@
 from lib import plotly as Plotly
 
 if __name__ == "__main__":
-    # plot = Plotly.Plotly("./examples/adddelay_singlerate_sensorless.csv")
-    # plot = Plotly("./examples/adddelay_singlerate_sensorless_100us.csv")
-    # plot = Plotly("./examples/singlerate_sensorless_100us.csv")
-    # plot = Plotly("./examples/singlerate_sensorless.csv")
-    # plot = Plotly("./examples/sample.csv")
-    plot = Plotly.Plotly("./examples/tmp_sch_results.csv")
+    # plot = Plotly.Plotly("./examples/tmp_sch_results.csv")
+    plot = Plotly.Plotly([
+        "./examples/singlerate_sensorless_100us.csv",
+        "./examples/singlerate_sensorless.csv"
+    ])
     plot.plot()

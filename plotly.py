127308771a70a70d1cb5f5c0fe9ebe297d1d8d4d
pokohide <hyde31415@gmail.com>
2018-02-28 15:10:27 +0900

    add examples

--- plotly.py
+++ plotly.py
@@ -1,9 +1,9 @@
 from lib import plotly as Plotly
 
 if __name__ == "__main__":
-    # plot = Plotly.Plotly("./examples/tmp_sch_results.csv", { "showTitle": False })
-    plot = Plotly.Plotly([
-        "./outputs/output_rate_0.csv",
-        "./outputs/output_rate_1.csv"
-    ])
+    plot = Plotly.Plotly("./outputs/fibo.csv", { "showTitle": False })
+    #plot = Plotly.Plotly([
+    #    "./outputs/output_rate_0.csv",
+    #    "./outputs/output_rate_1.csv"
+    #])
     plot.plot()
